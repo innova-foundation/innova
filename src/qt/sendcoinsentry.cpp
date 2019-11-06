d4d00db45b3951685374c87c58d61bc80eaa56ca
CircuitBreaker88 <sarris88p@gmail.com>
2019-11-06 15:29:07 -0500

    Edit Example Address and fix stake time in text

--- src/qt/sendcoinsentry.cpp
+++ src/qt/sendcoinsentry.cpp
@@ -24,7 +24,7 @@ SendCoinsEntry::SendCoinsEntry(QWidget *parent) :
 #if QT_VERSION >= 0x040700
     /* Do not move this to the XML file, Qt before 4.7 will choke on it */
     ui->addAsLabel->setPlaceholderText(tr("Enter a label for this address to add it to your address book"));
-    ui->payTo->setPlaceholderText(tr("Enter a Innova address (e.g. DyvGBZBowrppUwwNUo1GCRDTibzJi7g2M)"));
+    ui->payTo->setPlaceholderText(tr("Enter a Innova address (e.g. iZ4HRXTsLkp2Rty5RUHMLc6d5qVxVGifKJ)"));
     ui->narration->setPlaceholderText(tr("Enter a short note to send with payment (max 24 characters)"));
 #endif
     setFocusPolicy(Qt::TabFocus);
