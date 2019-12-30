598dd83659c579631d412e261a502a5b442cd8bd
Circuit Breaker <circuitbreaker@Circuits-MacBook-Pro.local>
2019-12-30 04:11:57 -0500

    make compatible for building for mac 10.8 level

--- build_osx.sh
+++ build_osx.sh
@@ -1,5 +1,5 @@
 qmake RELEASE=1 USE_UPNP=1 USE_QRCODE=1 USE_IPV6=1 innova-qt.pro
 make
-export QTDIR=/usr/local/Cellar/qt/5.14.0/
+export QTDIR=/Users/circuitbreaker/Qt5.3.2/5.3/clang_64/
 T=$(contrib/qt_translations.py $QTDIR/translations src/qt/locale)
 python2.7 contrib/macdeploy/macdeployqtplus -add-qt-tr $T -dmg -fancy contrib/macdeploy/fancy.plist Innova.app
