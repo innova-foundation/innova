5d57bcfa0cc8d6524cae0d48bef215422ea3ee27
CircuitBreaker <sarris88p@gmail.com>
2021-03-07 17:18:08 -0500

    Revert Snap

--- snapcraft.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Change line in innova-qt.pro"
-sed -i 's/LIBS += -lcurl -lssl -lcrypto -lcrypt32 -lssh2 -lgcrypt -lidn2 -lgpg-error -lunistring -lwldap32 -ldb_cxx$$BDB_LIB_SUFFIX/LIBS += -lcurl -lssl -lcrypto -ldb_cxx$$BDB_LIB_SUFFIX/' innova-qt.pro
