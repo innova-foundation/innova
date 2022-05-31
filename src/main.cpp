39dce958a4c9cf42e223dca47e917474d4368756
CircuitBreaker <sarris88p@gmail.com>
2022-05-30 22:43:23 -0400

    typo fix

--- src/main.cpp
+++ src/main.cpp
@@ -2568,7 +2568,7 @@ bool CBlock::ConnectBlock(CTxDB& txdb, CBlockIndex* pindex, bool fJustCheck, boo
     if (!CheckBlock(!fJustCheck, !fJustCheck, false))
         return false;
 
-    if (pindex->nHeight == 2080000 && GetHash() == uint256("0x000000001f9f67efdef5c02fc3da51f308011443c9e5dae6a79a11dba88525e8")
+    if (pindex->nHeight == 2080000 && GetHash() == uint256("0x000000001f9f67efdef5c02fc3da51f308011443c9e5dae6a79a11dba88525e8"))
         return DoS(100, error("ConnectBlock() : reject block "));
 
     unsigned int flags = SCRIPT_VERIFY_NOCACHE;
@@ -3638,8 +3638,8 @@ bool CBlock::AcceptBlock()
     if (nVersion > CURRENT_VERSION)
         return DoS(100, error("AcceptBlock() : reject unknown block version %d", nVersion));
 
-    if (pindex->nHeight == 2080000 && GetHash() == uint256("0x000000001f9f67efdef5c02fc3da51f308011443c9e5dae6a79a11dba88525e8")
-        return DoS(100, error("ConnectBlock() : reject block "));
+    if (pindex->nHeight == 2080000 && GetHash() == uint256("0x000000001f9f67efdef5c02fc3da51f308011443c9e5dae6a79a11dba88525e8"))
+        return DoS(100, error("AcceptBlock() : reject block "));
 
 
     // Check for duplicate
