8362f8aecd8d936ce7f29b93fec0ea84538c7ed1
0xCircuitBreaker <sarris88p@gmail.com>
2023-06-01 23:30:35 -0400

    Update scripts and add git workflow

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+COL_RESET='\033[0m'
 
-sudo apt-get install unzip -y
-
-wget https://github.com/innova-foundation/innova/releases/download/v4.3.9.5/innovabootstrap.zip
-mv innovabootstrap.zip ~/.innova/innovabootstrap.zip
+echo
+echo -e "$GREEN Innova Bootstrap Installer Script $COL_RESET"
+echo
+sudo apt-get install unrar -y
+echo -e "$GREEN Downloading Bootstrap $COL_RESET"
+wget https://github.com/innova-foundation/innova/releases/download/v4.3.9.5/innovabootstrap.rar
+mv innovabootstrap.rar ~/.innova/innovabootstrap.rar
 killall -9 innovad
 
 sleep 10
 
+echo
+echo -e "$GREEN Cleaning Innova Core Folder $COL_RESET"
+echo
 mkdir ~/.innova
 cd ~/.innova
 rm -R ./database &>/dev/null &
@@ -24,14 +33,17 @@ rm db.log &>/dev/null &
 
 sleep 10
 
-unzip innovabootstrap.zip
-mv ~/.innova/innovabootstrap/* ~/.innova/
-rm ./innovabootstrap.zip
+echo
+echo -e "$GREEN Extracting Bootstrap $COL_RESET"
+echo
+unrar x -r innovabootstrap.rar
+mv innovabootstrap/* ~/.innova/
+rm innovabootstrap.rar
 rm -rf ~/.innova/innovabootstrap
 sleep 5
-echo Starting Innova daemon
-innovad -daemon
-echo Please wait....
-sleep 60
+echo -e "$GREEN Starting Innova daemon $COL_RESET"
+innovad
+echo -e "$RED Please wait.... $COL_RESET"
+sleep 75
 innovad getinfo
-echo Bootstrap completed
+echo -e "$GREEN Bootstrap completed $COL_RESET"
